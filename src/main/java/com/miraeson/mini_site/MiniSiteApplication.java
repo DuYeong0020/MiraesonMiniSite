4d9300a6c1bb22004cb1e2b3f09022233dbfb7ab
DuYeong0020 <chlendud00@naver.com>
2021-07-11 13:25:22 +0900

    첫 프로젝트 구성 7/11

---
+++ src/main/java/com/miraeson/mini_site/MiniSiteApplication.java
@@ -0,0 +1,13 @@
+package com.miraeson.mini_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSiteApplication.class, args);
+    }
+
+}
