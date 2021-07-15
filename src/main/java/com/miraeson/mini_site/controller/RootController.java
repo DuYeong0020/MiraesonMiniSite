aeaa93d46ca57c4876d897ecf778208669f49b53
DuYeong0020 <chlendud00@naver.com>
2021-07-15 16:53:55 +0900

    RootController.redirectRoot 7/15

---
+++ src/main/java/com/miraeson/mini_site/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.miraeson.mini_site.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController implements ErrorController {
+
+    @GetMapping("/errror")
+    public String redirectRoot() {
+        return "index.html";
+    }
+}
